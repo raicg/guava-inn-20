d2731fa0cd30d8132bd4d6adce92907924159a62
Raí Gondim <raicg2@gmail.com>
2020-07-16 18:59:36 -0300

    fixing on reservations model: validate capacity of the room

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ApplicationRecord
   validates_presence_of :start_date, :end_date, :guest_name, :number_of_guests
   validates_numericality_of :number_of_guests, greater_than: 0, less_than_or_equal_to: 10
   validate :start_date_is_before_end_date
+  validate :room_with_insufficient_capacity
 
   def duration
     if start_date.present? && end_date.present? && end_date > start_date
@@ -19,6 +20,12 @@ class Reservation < ApplicationRecord
   end
 
   private
+
+  def room_with_insufficient_capacity
+    if room_id.present? && number_of_guests.present? && Room.find(room_id).capacity < number_of_guests
+      errors.add(:base, :invalid_number_of_guests, message: 'The selected room does not have enough capacity')
+    end
+  end
 
   def start_date_is_before_end_date
     if start_date.present? && end_date.present? && start_date >= end_date
