515b70da3a360d054b4210b1d0aab97cd9c119d2
Raí Gondim <raicg2@gmail.com>
2020-07-17 16:00:50 -0300

    creating reservations routing specs

---
+++ spec/routing/reservations_routing_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe ReservationsController, type: :routing do
+  describe "routing" do
+    it "routes to #new" do
+      expect(get: "/reservations/new").to route_to("reservations#new")
+    end
+
+    it "routes to #create" do
+      expect(post: "/reservations").to route_to("reservations#create")
+    end
+
+    it "routes to #destroy" do
+      expect(delete: "/reservations/1").to route_to("reservations#destroy", id: "1")
+    end
+
+    it "routes to #search" do
+      expect(get: "/reservations/search").to route_to("reservations#search")
+    end
+  end
+end
