9bc8ea4db76575bd6dabf05fff5d51a940e3125d
Raí Gondim <raicg2@gmail.com>
2020-07-17 15:58:21 -0300

    refactoring reservation model specs

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,157 +1,129 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  it 'validates presence of room' do
-    reservation = Reservation.new
+  let!(:room){ create(:room) }
+  let!(:reservation){ create(:reservation) }
 
-    expect(reservation).to_not be_valid
+  it 'validates presence of room' do
+    reservation.update(room_id: nil)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:room, :blank)
+    expect(reservation.room_id).to_not be_nil
   end
 
   it 'validates presence of start_date' do
-    reservation = Reservation.new
-
-    expect(reservation).to_not be_valid
+    reservation.update(start_date: nil)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:start_date, :blank)
+    expect(reservation.start_date).to_not be_nil
   end
 
   it 'validates presence of end_date' do
-    reservation = Reservation.new
-
-    expect(reservation).to_not be_valid
+    reservation.update(end_date: nil)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:end_date, :blank)
+    expect(reservation.end_date).to_not be_nil
   end
 
   it 'validates presence of guest_name' do
-    reservation = Reservation.new
-
-    expect(reservation).to_not be_valid
+    reservation.update(guest_name: nil)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:guest_name, :blank)
+    expect(reservation.guest_name).to_not be_nil
   end
 
   it 'validates presence of number_of_guests' do
-    reservation = Reservation.new
-
-    expect(reservation).to_not be_valid
+    reservation.update(number_of_guests: nil)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:number_of_guests, :blank)
+    expect(reservation.number_of_guests).to_not be_nil
   end
 
   it 'validates that number_of_guests should not be zero' do
-    reservation = Reservation.new(number_of_guests: 0)
-
-    expect(reservation).to_not be_valid
+    reservation.update(number_of_guests: 0)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:number_of_guests, :greater_than)
+    expect(reservation.number_of_guests).to_not eq(0)
   end
 
   it 'validates that number_of_guests should not be negative' do
-    reservation = Reservation.new(number_of_guests: -1)
-
-    expect(reservation).to_not be_valid
+    reservation.update(number_of_guests: -1)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:number_of_guests, :greater_than)
+    expect(reservation.number_of_guests).to_not eq(-1)
   end
 
   it 'validates that number_of_guests should not be greater than ten' do
-    reservation = Reservation.new(number_of_guests: 11)
-
-    expect(reservation).to_not be_valid
+    reservation.update(number_of_guests: 20)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:number_of_guests, :less_than_or_equal_to)
+    expect(reservation.number_of_guests).to_not eq(20)
   end
 
   it 'validates that start_date is before end_date' do
-    reservation = Reservation.new(start_date: '2020-08-02', end_date: '2020-08-01')
-
-    expect(reservation).to_not be_valid
+    reservation.update(start_date: Date.tomorrow, end_date: Date.today)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:base, :invalid_dates)
+    expect(reservation.start_date).to_not eq(Date.tomorrow)
+    expect(reservation.end_date).to_not eq(Date.today)
   end
 
   it 'validates that start_date is not equal to end_date' do
-    reservation = Reservation.new(start_date: '2020-08-01', end_date: '2020-08-01')
-
-    expect(reservation).to_not be_valid
+    reservation.update(start_date: Date.tomorrow, end_date: Date.tomorrow)
+    reservation.reload
+    
     expect(reservation).to have_error_on(:base, :invalid_dates)
+    expect(reservation.start_date).to_not eq(Date.tomorrow)
+    expect(reservation.end_date).to_not eq(Date.tomorrow)
   end
 
   describe '#duration' do
     it 'returns the number of nights for the reservation' do
-      reservation = Reservation.new(start_date: '2020-08-01', end_date: '2020-08-05')
+      reservation.update(start_date: Date.tomorrow, end_date: Date.tomorrow + 4.days)
+      reservation.reload
 
       expect(reservation.duration).to eq(4)
-    end
-
-    context 'when start or end_date is blank' do
-      it 'returns nil' do
-        reservation = Reservation.new(start_date: '2020-08-01', end_date: nil)
-
-        expect(reservation.duration).to be_nil
-      end
-    end
-
-    context 'when the start_date is equal to or after the end_date' do
-      it 'returns nil' do
-        reservation = Reservation.new(start_date: '2020-08-01', end_date: '2020-07-31')
-
-        expect(reservation.duration).to be_nil
-      end
     end
   end
 
   describe '#code' do
     it 'returns the room code and two-digit reservation id' do
-      room = Room.new(code: '101')
-      reservation = Reservation.new(id: 2, room: room)
+      formatted_id = '%02d' % reservation.id
 
-      expect(reservation.code).to eq('101-02')
-    end
-
-    context 'when the room is not present' do
-      it 'returns nil' do
-        reservation = Reservation.new(room: nil)
-
-        expect(reservation.code).to be_nil
-      end
-    end
-
-    context 'when the room is present but does not have code' do
-      it 'returns nil' do
-        room = Room.new(code: nil)
-        reservation = Reservation.new(id: 2, room: room)
-
-        expect(reservation.code).to be_nil
-      end
-    end
-
-    context 'when the reservation does not have id' do
-      it 'returns nil' do
-        room = Room.new(code: '101')
-        reservation = Reservation.new(id: nil, room: room)
-
-        expect(reservation.code).to be_nil
-      end
+      expect(reservation.code).to eq("#{room.code}-#{formatted_id}")
     end
 
     context 'when the reservation id is greater than 99' do
       it 'returns a code with the second part having more than two digits' do
-        room = Room.new(code: '101')
-        reservation = Reservation.new(id: 100, room: room)
+        reservation.update(id: 123)
+        reservation.reload
 
-        expect(reservation.code).to eq('101-100')
+        expect(reservation.code).to eq("#{room.code}-123")
       end
     end
 
     context 'when the room is already reserved' do
-      it 'should not persist the new reservation' do
-        room = Room.create(code: '101')
-        reservation = Reservation.create(start_date: '2020-08-01', end_date: '2020-08-10', number_of_guests: 1, guest_name: 'name', room_id: room.id)
-        reservation2 = Reservation.create(start_date: '2020-08-04', end_date: '2020-08-07', number_of_guests: 1, guest_name: 'name', room_id: room.id)
+      it 'should not accept the dates of the new reservation' do
+        reservation2 = Reservation.create(reservation.attributes)
 
+        expect(reservation2).to have_error_on(:base, :invalid_dates)
         expect(reservation2).to_not be_persisted
       end
     end
 
     it 'remaing_days_until_custom_date method should returns the remaing days' do
-      reservation = Reservation.new(start_date: '2020-08-01', end_date: '2020-08-10')
+      reservation.update(start_date: Date.tomorrow, end_date: Date.tomorrow + 2.days)
 
-      expect(reservation.remaing_days_until_custom_date(Date.parse('2020-08-6'))).to eq(5)
+      expect(reservation.remaing_days_until_custom_date(Date.tomorrow + 1.day)).to eq(1)
     end
   end
 end
